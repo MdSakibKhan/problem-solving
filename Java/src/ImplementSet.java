ffec204b49d5930228e1d1fc6259c61d1461a265
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-10-20 23:27:24 +0600

    Implement Set

---
+++ Java/src/ImplementSet.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+public class ImplementSet {
+    public static void main(String []args){
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(2);
+        set.add(3);
+        set.add(4);
+
+        for(int i : set) System.out.println(i);
+    }
+}
