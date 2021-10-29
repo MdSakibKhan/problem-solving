3a42d85e3a19d526bd193d910fb12e765dc24d6d
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-10-30 00:42:53 +0600

    Interface example added for mobile class

--- Java/src/Mobile.java
+++ Java/src/Mobile.java
@@ -1,8 +1,12 @@
-public class Mobile {
+public class Mobile implements MobileInterface{
     Mobile next ;
     String name ;
+    String NN;
     Mobile(String name , Mobile mobile){
         this.name = name;
         this.next = mobile;
+    }
+    public String getName(){
+        return this.name;
     }
 }
---
+++ Java/src/MobileInterface.java
@@ -0,0 +1,3 @@
+public interface MobileInterface {
+    String getName();
+}
