3e9225fe9be9b63cab9324974fc68ee0dd913474
Md. Sakib Khan <36070242+MdSakibKhan@users.noreply.github.com>
2021-10-30 02:08:14 +0600

    String validation check

---
+++ Java/src/ValidStringCheck.java
@@ -0,0 +1,19 @@
+public class ValidStringCheck {
+    public static void main(String[] args) {
+        String text1= "_geeks"; // valid
+        String text2= "0_geeks"; // invalid
+        System.out.println(isValidString(text1));
+        System.out.println(isValidString(text2));
+
+    }
+
+    private static boolean isValidString(String text) {
+
+        if(!(Character.isAlphabetic(text.charAt(0)) || text.charAt(0)=='_')) return false;
+
+        for(int index = 1 ; index<text.length() ; index++){
+            if(!(Character.isAlphabetic(text.charAt(index)) || text.charAt(index)=='_' || Character.isDigit(text.charAt(index)) )) return false;
+        }
+        return true;
+    }
+}
